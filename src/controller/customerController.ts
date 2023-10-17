3e8eff39c53edf29865cb8e57a72be1604389749
Mitesh savaliya <msavaliya@plusonex.com>
2023-10-17 15:46:12 +0530

    optional dependents

--- src/controller/customerController.ts
+++ src/controller/customerController.ts
@@ -9,7 +9,7 @@ import Request from '../interface';
  * */
 
 export const createCustomer = asyncHandler(async (req: Request, res: Response) => {
-  const { firstName, lastName, aadharNumber, image, gender,dateOfBirth, agentId,address, dependentList } = req.body;
+  const { firstName, lastName, aadharNumber, image, gender, dateOfBirth, agentId, address, dependentList } = req.body;
   // const customerIsExist = await Customer.findOne({ aadharNumber });
   // const dependentsIsExist = await Dependent.find({ aadharNumber });
   // if (customerIsExist) {
@@ -20,18 +20,18 @@ export const createCustomer = asyncHandler(async (req: Request, res: Response) =
   //   res.status(StatusCodes.BAD_REQUEST).json({ message: "Customer already exist with this aadhar number already registered with dependents" });
   //   return;
   // }
-  
-  const dependents = dependentList.map((dependent: IDpendent) => {
+
+  const dependents = dependentList?.map((dependent: IDpendent) => {
     return {
-      firstName: dependent.firstName,
-      lastName: dependent.lastName,
-      aadharNumber: dependent.aadharNumber,
-      dateOfBirth: dependent.dateOfBirth,
-      image: dependent.image,
-      gender: dependent.gender,
-      address: dependent.address,
-      relation: dependent.relation,
-      relationShip: dependent.relationShip,
+      firstName: dependent?.firstName,
+      lastName: dependent?.lastName,
+      aadharNumber: dependent?.aadharNumber,
+      dateOfBirth: dependent?.dateOfBirth,
+      image: dependent?.image,
+      gender: dependent?.gender,
+      address: dependent?.address,
+      relation: dependent?.relation,
+      relationShip: dependent?.relationShip,
     }
   });
   if (dependents && dependents.length > 5) {
@@ -51,11 +51,11 @@ export const createCustomer = asyncHandler(async (req: Request, res: Response) =
   //   res.status(StatusCodes.BAD_REQUEST).json({ message: "Customer already exist with this aadhar number already registered with dependents" });
   //   return;
   // }
-  const dependentsArray = await Dependent.insertMany(dependents);
-  const dependentsIds = dependentsArray.map((dependent) => { 
-    return dependent._id; 
-  });
-  const customer = await Customer.create({ firstName, lastName, aadharNumber, image, agentId,dateOfBirth,gender, address, dependents: dependentsIds });
+  const dependentsArray = dependentList && dependentList.length > 0 ? await Dependent.insertMany(dependents) : [];
+  const dependentsIds = dependentList && dependentList.length > 0 ? dependentsArray?.map((dependent) => {
+    return dependent?._id;
+  }) : []
+  const customer = await Customer.create({ firstName, lastName, aadharNumber, image, agentId, dateOfBirth, gender, address, dependents: dependentsIds });
   const data = {
     customer: customer,
     dependents: dependentsArray
@@ -82,7 +82,7 @@ export const getAllCustomers = asyncHandler(async (req: Request, res: Response) 
     })
   );
 
-  res.status(StatusCodes.OK).json({count: customers.length, customerDetailInDetails });
+  res.status(StatusCodes.OK).json({ count: customers.length, customerDetailInDetails });
 });
 
 
